d382641297a4e7490845fbb6337b6aa12ca1fed5
ronidalmolin79 <ronidalmolin79@gmail.com>
2022-11-10 19:50:36 -0400

    inserção tabela de dados

---
+++ modules/service/TodoService.js
@@ -0,0 +1,95 @@
+import { insert, uptdate, remove, listAll } from 'modules/data/TodoDAO'
+
+const handlePost = async (reqParams) => {
+    const result = {
+        status: 200,
+        data: {}
+    }
+
+    try {
+        const insertedObj = await insert(reqParams.data)
+        result.data = insertedObj
+    } catch (error) {
+        result.status = 500,
+            result.data = error.message
+    }
+
+    return result
+}
+
+const handlePut = async (reqParams) => {
+    const result = {
+        status: 200,
+        data: {}
+    }
+
+    try {
+        const uptdateObj = await uptdate(reqParams.data)
+        result.data = uptdateObj
+    } catch (error) {
+        result.status = 500,
+            result.data = error.message
+    }
+
+    return result
+}
+
+const handleRemove = async (reqParams) => {
+    const result = {
+        status: 200,
+        data: {}
+    }
+
+    try {
+        const todoId = parseInt(reqParams.query.id)
+        const removedObj = await remove(todoId)
+        result.data = removedObj
+    } catch (error) {
+        result.status = 500,
+            result.data = error.message
+    }
+
+    return result
+}
+
+const handleGet = async () => {
+    const result = {
+        status: 200,
+        data: {}
+    }
+
+    try {
+        const todoList = await listAll()
+        if (todoList.length == 0) {
+            result.status = 204
+        } else {
+            result.data = todoList
+        }
+    } catch (error) {
+        result.status = 500,
+            result.data = error.message
+    }
+
+    return result
+}
+
+const acceptedMethods = {
+    POST: handlePost,
+    PUT: handlePut,
+    DELETE: handleRemove,
+    GET: handleGet
+}
+
+const handleRequest = async (reqParams) => {
+    if (acceptedMethods[reqParams.method] !== undefined) {
+        let requestMethod = acceptedMethods[reqParams.method]
+        return await requestMethod(reqParams)
+    } else {
+        return {
+            status: 404,
+            data: {}
+        }
+    }
+}
+
+export { handleRequest }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,48 @@
+import useSWR from "swr"
+import dayjs from "dayjs"
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+const tableRowItem = function (item) {
+  return (
+    <tr>
+      <td>{item.id}</td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{dayjs(item.createdAt).format("DD/MM/YYYY HH:mm")}</td>
+      <td>{item.done ? 'Sim' : 'Não'}</td>
+      <td>{item.finishedAt}</td>
+    </tr>
+  )
+}
 
 export default function Home() {
+
+  const { data, error } = useSWR('/api/todos', fetcher)
+
+  if (!data) return <div>Carregando...</div>
+
   return (
-    <div>Olá Mundo</div>
+    <div>
+      <table>
+        <thead>
+
+          <tr>
+            <th>Id</th>
+            <th>Titulo</th>
+            <th>Descricao</th>
+            <th>Criado em</th>
+            <th>Finalizado</th>
+            <th>Finalizado em</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+          {data.map((item) => tableRowItem(item))}
+        </tbody>
+
+      </table>
+    </div>
   )
 }
