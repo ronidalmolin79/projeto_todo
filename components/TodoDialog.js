d1161006dab902863dd2f4c5c0d55dc593ed3762
ronidalmolin79 <ronidalmolin79@gmail.com>
2022-11-24 20:24:01 -0400

    alteracoes

--- components/TodoDialog.js
+++ components/TodoDialog.js
@@ -14,10 +14,11 @@ const updateTodo = (title, description, todo) => {
 }
 
 export default function TodoDialog(props) {
-    if (props.isOpen === true) {
 
-        const [title, setTitle] = useState(props.item.title)
-        const [description, setDescription] = useState(props.item.description)
+    const [title, setTitle] = useState(props.item.title)
+    const [description, setDescription] = useState(props.item.description)
+
+    if (props.isOpen === true) {
 
         return (
             <div className="todo-dialog">
@@ -37,5 +38,7 @@ export default function TodoDialog(props) {
                 </div>
             </div>
         )
+    } else {
+        return <></>
     }
 }
