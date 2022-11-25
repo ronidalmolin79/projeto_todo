82a9723e3f0ff8467d1ed7f0e54661cdaa78e75f
ronidalmolin79 <ronidalmolin79@gmail.com>
2022-11-25 19:57:54 -0400

    melhorias performance

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useSWRConfig } from 'swr'
 
-const postNewTodo = function (todo) {
-
-    fetch('/api/todos', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(todo),
-    })
+const postNewTodo = function (todo, mutate) {
+    mutate('/api/todos', async todos => {
+        let result = await fetch('/api/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todo),
+        })
+        let insertedTodo = await result.json()
+        let filteredData = todos.filter(todo => todo.id !== insertedTodo.id)
+        return [...filteredData, insertedTodo]
+    }, { revalidate: false })
 }
 
 const checkKeyPressed = function (event, callbackFunc) {
     if (event.defaultPrevented) return;
-    let key = event.key || event.KeyCode;
+    let key = event.key || event.keyCode;
     if (key === 'Enter' || key === 13) callbackFunc();
 }
 
-export default function TodoForm() {
+export default function TodoForm(props) {
+
+    const { mutate } = useSWRConfig()
 
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
@@ -26,16 +33,17 @@ export default function TodoForm() {
         <div className='todo-form input-group'>
             <input className='title-input'
                 type="text"
-                placeholder="O que voce precisa fazer?"
+                placeholder="O que você precisa fazer?"
                 value={title}
                 onChange={e => setTitle(e.target.value)}
                 onKeyUp={(e) => checkKeyPressed(e, () => {
-                    postNewTodo({ title, description })
+                    postNewTodo({ title, description }, mutate)
                     setTitle("")
                 })} />
 
-            <button className="add-button" onClick={() => {
-                postNewTodo({ title, description })
+
+            <button className='add-button' onClick={() => {
+                postNewTodo({ title, description }, mutate)
                 setTitle("")
             }}>
 
@@ -48,4 +56,3 @@ export default function TodoForm() {
         </div>
     )
 }
-
